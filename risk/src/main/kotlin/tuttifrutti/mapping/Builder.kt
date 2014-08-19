ba637cdb112c610743f4bdc9e2cc2569b693a40b
Tim Clark <t.i.j.clark@gmail.com>
2014-08-19 21:29:21 +0100

    Rename class

--- risk/src/main/kotlin/tuttifrutti/mapping/Builder.kt
+++ risk/src/main/kotlin/tuttifrutti/mapping/Builder.kt
@@ -9,12 +9,12 @@ data class Words(val text: String, val label: String? = null) : Field
 data class Code(val text: String, val code: String, val children: List<Field> = listOf()) : Field
 data class Choice(val text: String, val codes: List<Code>, val label: String? = null) : Field
 
-enum class Positive {
+enum class Selected {
 	First
 	Second
 }
 
-data class Confirmation(val text: String, val first: Code, val second: Code, val positive: Positive, val label: String? = null) : Field
+data class Confirmation(val text: String, val first: Code, val second: Code, val positive: Selected, val label: String? = null) : Field
 data class Collection(val items: List<Field>, val label: String? = null) : Field
 data class Group(val items: List<Field>, val label: String? = null) : Field
 
